246c465b1317aca3f80217ac43446b93398c99c5
Nicol-Huaiquil <72150599+Nicol-Huaiquil@users.noreply.github.com>
2022-09-26 22:32:47 -0300

    imagen en local no funca

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Button, Spacer } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
+import { Image } from "@nextui-org/react";
 
 function Home(){
     const navigate = useNavigate();
     return (
       <div className="App">
         Home
+        
+        <Image
+          width={320}
+          height={180}  
+          src="https://github.com/WsDoragon/Gestion_usuario_2.0/blob/main/P%C3%A1gina-Gesti%C3%B3n/src/assets/logoA.png?raw=true"
+          alt="Default Image"
+          objectFit="cover"
+        />
+
         <Spacer y={0.5} />
         <Button onClick={() => {navigate("/page1")}} >Botón 1</Button>
         <Spacer y={0.5} />
