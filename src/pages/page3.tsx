529031fe45ce3a49b3806a9398e5c4d49fd1e2e7
Nicol-Huaiquil <72150599+Nicol-Huaiquil@users.noreply.github.com>
2022-09-26 23:02:09 -0300

    pruebas en page3

--- src/pages/page3.tsx
+++ src/pages/page3.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { Grid, Card, Text } from "@nextui-org/react";
 
 function Page3(){
+    interface prueba {
+        text:string
+    }
 
-    return(
-        <div>
-            Página 3 
-            Intento de cambio de página forzado
-        </div>
-    );
+    const MockItem = ({ text }:prueba|any) => {
+        return (
+          <Card css={{ h: "$24", $$cardColor: '$colors$primary' }}>
+            <Card.Body>
+              <Text h6 size={15} color="white" css={{ mt: 0 }}>
+                {text}
+              </Text>
+            </Card.Body>
+          </Card>
+        );
+      };
+      return (
+            <Card>
+                <div>
+                    Página 3 
+                    Intento de cambio de página forzado
+                </div>
+             </Card>
+      );
 }
 
 export default Page3;
