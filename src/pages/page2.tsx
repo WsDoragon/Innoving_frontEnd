4e8295cf0883f8c4a48c23ccf75072881552ec9d
Nicol-Huaiquil <72150599+Nicol-Huaiquil@users.noreply.github.com>
2022-10-01 23:56:11 -0300

    trabajando en submenu
    intentando desplegar componentes en onclick

--- src/pages/page2.tsx
+++ src/pages/page2.tsx
@@ -1,37 +1,42 @@
-import React from "react";
-import { Button, Spacer, Grid } from "@nextui-org/react";
+import React, { Component } from "react";
+import { Grid,Navbar, Button, Link, Card } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import Header from "./test_pages/Header_test";
-import Barrita from '../components/barrita'
-import Btn1 from '../components/btn1'
-import {Fragment} from 'react';
-import { Collapse, Text } from "@nextui-org/react";
+
+function handler(){
+    return(
+        <h3>
+         Administrador
+       </h3>
+    )
+}
 
 function Analista(){
     const navigate = useNavigate();
     return(
-        
-        <Collapse.Group>
-            <Grid.Container gap={1}>
-                <Collapse title="Option A">
-                    <Text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat.
-                    </Text>
-                </Collapse>
-           
-                <Collapse title="Option B">
-                    <Text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat.
-                    </Text>
-                </Collapse>
-            </Grid.Container> 
-    </Collapse.Group>
+        <Grid.Container gap={2} justify="center">
+            <Header/>
+
+            <Grid xs={12}>
+                <Navbar variant="sticky">
+                    <Navbar.Content variant="highlight" hideIn="xs">
+                        <Navbar.Link href="#">Indicadores</Navbar.Link>
+                        <Navbar.Link isActive href="#">Solicitudes</Navbar.Link>
+                        <Navbar.Item>
+                            <Button onClick={()=><h3>admin</h3>} auto flat as={Link} href="#">
+                                prueba1
+                            </Button>
+                        </Navbar.Item>
+                        <Navbar.Item>
+                            <Button auto flat as={Link} href="#">
+                                prueba2
+                            </Button>
+                        </Navbar.Item>
+                    </Navbar.Content>
+                </Navbar>
+            </Grid>
+
+        </Grid.Container>
     );
 }
 
