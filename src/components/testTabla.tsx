2e76f38ee121b17ade80857a1a84d55bc07e2cb4
Jorge Gonzalez <WsDoragon@users.noreply.github.com>
2022-10-04 21:53:52 -0300

    Agregado boton de crear usuario y editar

--- src/components/testTabla.tsx
+++ src/components/testTabla.tsx
@@ -1,12 +1,16 @@
 import axios from 'axios';
 //@ts-ignore
-import { Table, Row, Col, Tooltip, User, Text, Button } from "@nextui-org/react";
+import { Table, Row, Col, Tooltip, User, Text, Button, Link, Spacer } from "@nextui-org/react";
 import algo from './Axiostabla';
 import { StyledBadge } from "../styledIcons/StyledBadge";
 import { IconButton } from "../styledIcons/IconButton";
 import { EyeIcon } from "../styledIcons/EyeIcon";
 import { EditIcon } from "../styledIcons/EditIcon";
 import { DeleteIcon } from "../styledIcons/DeleteIcon";
+
+import { useNavigate } from "react-router-dom";
+
+
 
 type UserType = {
     rut: string
@@ -23,6 +27,8 @@ type GetUsersResponse = {
 const data = algo()
 
 export default function TestTabla() {
+  const navigate = useNavigate();
+
     const columns = [
         {
             key: "rut",
@@ -49,61 +55,69 @@ export default function TestTabla() {
         { name: "ACTIONS", uid: "rut" },
     ]
 
-    const users: UserType[] = [{rut: "20318537-5", nombre: "Jorge", apellido: "Gonzalez", correo: "jorge@uach", pass: "123456" }, {rut: "ola", nombre: "a", apellido: "b", correo: "c", pass: "d" }]
+    const users: UserType[] = [
+      {rut: "20318537-5", nombre: "Jorge", apellido: "Gonzalez", correo: "jorge@uach.cl", pass: "123456" }, 
+      {rut: "10563123-6", nombre: "Pepito", apellido: "Jimenez", correo: "PJimenez@uach.cl", pass: "123456" }
+    ]
     
 
     return(
-        <Table
-        aria-label="Example table with dynamic content"
-        css={{
-          height: "auto",
-          minWidth: "100%",
-        }}
-    >
-        <Table.Header columns={columns}>
-        {(column) => (
-          <Table.Column 
-            key={column.key}
-            hideHeader={column.key === "actions"}
-            align={column.key === "actions" ? "center" : "start"}
-            >
-                {column.label}</Table.Column>
-        )}
-      </Table.Header>
-      <Table.Body items={users}>
-        {(item) => (
-          <Table.Row key={item.rut}>
-            <Table.Cell><Text b size={14}>{item[`rut`]} </Text></Table.Cell>
-            <Table.Cell><Text b size={14}>{item[`nombre`]} {item[`apellido`]}</Text></Table.Cell>
-            <Table.Cell><Text b size={14}>{item[`correo`]}</Text></Table.Cell>
-            
-            <Table.Cell> 
-                <Row justify="center" align="center">
-                
-                <Col css={{ d: "flex" }}>
-                <Tooltip content="Editar Usuario">
-                    <Button onClick={() => console.log("editar usuario", item.rut)}>
-                    <EditIcon size={20} fill="#979797" />
-                    </Button>
-                </Tooltip>
-                </Col>
-                <Col css={{ d: "flex" }}>
-                <Tooltip
-                    content="Desactivar usuario"
-                    color="error"
-                    onClick={() => console.log("Desactivar usuario", item.rut)}
-                >
-                    <Button>
-                        Desactivar
-                    </Button>
-                </Tooltip>
-                </Col>
-            </Row></Table.Cell>
-                
-                
-                
-          </Table.Row>
-        )}
-      </Table.Body>
-        </Table>
+      <div>
+        <Button onClick={() => {navigate("/crearUser")}} as={Link} href="#" >Crear nuevo usuario</Button>
+          <Table
+          aria-label="Example table with dynamic content"
+          css={{
+            height: "auto",
+            minWidth: "100%",
+          }}
+      >
+          <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column 
+              key={column.key}
+              hideHeader={column.key === "actions"}
+              align={column.key === "actions" ? "center" : "start"}
+              >
+                  {column.label}</Table.Column>
+          )}
+        </Table.Header>
+        <Table.Body items={users}>
+          {(item) => (
+            <Table.Row key={item.rut}>
+              <Table.Cell><Text b size={14}>{item[`rut`]} </Text></Table.Cell>
+              <Table.Cell><Text b size={14}>{item[`nombre`]} {item[`apellido`]}</Text></Table.Cell>
+              <Table.Cell><Text b size={14}>{item[`correo`]}</Text></Table.Cell>
+              
+              <Table.Cell> 
+                  <Row justify="center" align="center">
+                  
+                  <Col css={{ d: "flex" }}>
+                  <Tooltip content="Editar Usuario">
+                      <Button onClick={() => {navigate(`/editarUser/${item.rut}`)}} as={Link} href="#">
+                        Editar
+                      {/*<EditIcon size={20} fill="#979797" />*/}
+                      </Button>
+                  </Tooltip>
+                  </Col>
+                  <Spacer x={0.5}/>
+                  <Col css={{ d: "flex" }}>
+                  <Tooltip
+                      content="Desactivar usuario"
+                      color="error"
+                      onClick={() => console.log("Desactivar usuario", item.rut)}
+                  >
+                      <Button color={"error"}>
+                          Desactivar
+                      </Button>
+                  </Tooltip>
+                  </Col>
+              </Row></Table.Cell>
+                  
+                  
+                  
+            </Table.Row>
+          )}
+        </Table.Body>
+          </Table>
+        </div>
 )}
