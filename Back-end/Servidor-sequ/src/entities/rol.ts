4524015adb453c1669648e0203c58a6ad4128557
Doragon <berna2022@gmail.com>
2022-10-26 09:12:05 -0300

    Inicio de repo

--- Back-end/Servidor-sequ/src/entities/rol.ts
+++
@@ -1,9 +0,0 @@
-export class Rol {
-    public id: number;
-    public nombre: string;
-
-    constructor(nombre: string) {
-        this.nombre = nombre;
-
-    }
-}
--- Back-end/Servidor-sequ/src/entities/rol_usuario.ts
+++
@@ -1,4 +0,0 @@
-export class Rol {
-    public id_rol: number;
-    public id_usuario: string;
-}
