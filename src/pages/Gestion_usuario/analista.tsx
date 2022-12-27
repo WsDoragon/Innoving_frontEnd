5108dfcc8f4306dd89e7f7be141d7a6d887ebe54
Bastian1212 <bastianvillanueva@me.com>
2022-12-26 22:26:19 -0300

    a

--- src/pages/Gestion_usuario/analista.tsx
+++ src/pages/Gestion_usuario/analista.tsx
@@ -20,8 +20,6 @@ function Analista() {
                 <Spacer y={0.5} />
                 <Button onPress={() => navigate("./metas")} auto flat as={Link} href="#"> Metas </Button>
                 <Spacer y={0.5} />
-                <Button onPress={() => navigate("./metricas")} auto flat as={Link} href="#" ></Button>
-                <Spacer y={0.5} />
                 <Button onPress={() => navigate("./evidencias")} auto flat as={Link} href="#"> Evidencias </Button>
                 <Spacer y={0.5} />
                 <Button onPress={() => navigate("./prueba/indicador/M25")} auto flat as={Link} href="#" > {" "}Analisis Indicadores{" "}</Button>
